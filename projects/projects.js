bd9623024b05cb35442ceb8db4a008041629b0c7
CSAaditya <43223725+CSAaditya@users.noreply.github.com>
2026-04-25 21:35:57 -0700

    Catching up
    Catching up from past labs

---
+++ projects/projects.js
@@ -0,0 +1,11 @@
+import { fetchJSON, renderProjects } from '../global.js';
+
+const projects = await fetchJSON('../lib/projects.json');
+const projectsContainer = document.querySelector('.projects');
+
+const titleEl = document.querySelector('.projects-title');
+if (titleEl) {
+  titleEl.textContent = `${projects.length} Projects`;
+}
+
+renderProjects(projects, projectsContainer, 'h2');
